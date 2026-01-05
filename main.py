46b8e0f7dfa3fabab609ca594d1f21c8d15a00d2
Euan Jones <euanefjones@gmail.com>
2026-01-05 08:14:29 +0000

    Intial Commit - v2

--- main.py
+++ main.py
@@ -1,95 +1,114 @@
-from PIL import Image # Depends on the Pillow lib
 from opensimplex import OpenSimplex
-import math
+import numpy as np
+from PIL import Image
 import uuid
 import os
 
-print("Initiated\nBegin World Generation...")
-print("  ...Defining Constants...")
 
-WIDTH = 256
-HEIGHT = 256
-FEATURE_SIZE = 50.0
-SEED = uuid.uuid1().int >> 64
-NOISE = OpenSimplex(SEED)
-
-COLOR_DEEPWATER = (0, 62, 178)
-COLOR_WATER = (9, 82, 198)
-COLOR_SAND = (254, 224, 179)
-COLOR_GRASS = (9, 120, 93)
-COLOR_DARKGRASS = (10, 107, 72)
-COLOR_DARKESTGRASS = (11, 94, 51)
-COLOR_DARKROCKS = (140, 142, 123)
-COLOR_ROCKS = (160, 162, 143)
-COLOR_SNOW = (255, 255, 255)
+COLORS = {
+  "DEEPWATER" : (0, 62, 178),
+  "WATER" : (9, 82, 198),
+  "SHORE" : (20, 100, 210),
+  "SAND" : (254, 224, 179),
+  "GRASS" : (9, 120, 93),
+  "DARKGRASS" : (10, 107, 72),
+  "DARKESTGRASS" : (11, 94, 51),
+  "DARKROCKS" : (140, 142, 123),
+  "ROCKS" : (160, 162, 143),
+  "SNOW" : (255, 255, 255)
+}
 
 
-#gradientvalue = 128 - (abs((x - WIDTH/2)/2) + abs(( y - HEIGHT/2)/2))
-#value = gradientvalue + noisevalue + 64
+def getColor(heightVal):
+    if heightVal < 0:
+        heightVal = 0
+    if heightVal > 255:
+        heightVal = 255
 
-def FindColor(value):
-  if value <= 84:
-        return COLOR_DEEPWATER
-  elif value <= 102:
-        return COLOR_WATER
-  elif value <= 112:
-        return COLOR_SAND
-  elif value <= 134:
-        return COLOR_GRASS
-  elif value <= 164:
-        return COLOR_DARKGRASS
-  elif value <= 200:
-        return COLOR_DARKESTGRASS
-  elif value <= 224:
-        return COLOR_DARKROCKS
-  elif value <= 242:
-        return COLOR_ROCKS
-  elif value <= 255:
-        return COLOR_SNOW
-  return COLOR_DEEPWATER
+    if heightVal <= 84:
+        return COLORS["DEEPWATER"]
+    elif heightVal <= 98:
+        return COLORS["WATER"]
+    elif heightVal <= 110:
+        return COLORS["SHORE"]
+    elif heightVal <= 115:
+        return COLORS["SAND"]
+    elif heightVal <= 134:
+        return COLORS["GRASS"]
+    elif heightVal <= 164:
+        return COLORS["DARKGRASS"]
+    elif heightVal <= 200:
+        return COLORS["DARKESTGRASS"]
+    elif heightVal <= 224:
+        return COLORS["DARKROCKS"]
+    elif heightVal <= 242:
+        return COLORS["ROCKS"]
+    elif heightVal <= 255:
+        return COLORS["SNOW"]
+    return COLORS["DEEPWATER"]
 
-  return value
-  
-def main():
-      print("  ...Begin Main Process...")
-      print("    ...creating canvases...")
-      imnoise = Image.new('P', (WIDTH, HEIGHT))
-      imgradient = Image.new('P', (WIDTH, HEIGHT))
-      imblended = Image.new('P', (WIDTH, HEIGHT))
-      print("    ...generating values...")
 
-  
-      for y in range(0, HEIGHT):
-            for x in range(0, WIDTH):
-                  noisevalue = (NOISE.noise2(x / FEATURE_SIZE, y / FEATURE_SIZE)+1)* 128
-                  distX = WIDTH/2 - abs(WIDTH / 2 - x)
-                  distY = HEIGHT/2 -abs(HEIGHT / 2 - y)
-                  dist = min(distX, distY)
-                  gradientvalue = dist / max(WIDTH/2, HEIGHT/2) * 255
-                  imnoise.putpixel((x, y), FindColor(noisevalue))
-                  imgradient.putpixel((x, y), FindColor(gradientvalue))
-                  imblended.putpixel((x, y), FindColor(noisevalue/2 + gradientvalue/2))
 
-      print("    ...saving outputs...")
+def generateMap(height, width, seed, scale, octaves, bias, persistance, lacunarity, name):
+    heightMap = np.empty((height, width), dtype=np.short)
+    noise = OpenSimplex(seed=seed)
+    for y in range(0, height):
+        for x in range(0, width):
+            amplitude = 1
+            frequency = 1
+            noiseHeight = 0
 
+          
+            for octave in range(0, octaves):
+                sampleX = x / scale * frequency
+                sampleY = y / scale * frequency
+
+                value = noise.noise2(sampleX, sampleY)
+                noiseHeight += value * amplitude
+
+                amplitude *= persistance
+                frequency *= lacunarity
+                heightMap[y][x] = (noiseHeight + 1) * 128
+
+           
+            distX = abs(width / 2 - x)
+            distY = abs(height / 2 - y)
+            dist = max(distX, distY)
+            maxWidth = width / 2 - bias
+            delta = dist / maxWidth
+            gradient = delta ** 2
+            heightMap[y][x] *= max(0.0, 1.0 - gradient)
+
+
+            
+
+        os.system("cls")
+        print("Generating Map...\n(" + str(height) + "px X " + str(width) + "px)\n--" + str(int(y/height*100))+"%--")
+      
+    os.system("cls")
+    print("Generating Map...\n(" + str(height) + "px X " + str(width) + "px)\n--Done!--")
+    colourMap = np.zeros((height, width, 3), dtype=np.uint8)
+
+    for y in range(0, height):
+        for x in range(0, width):
+            colourMap[y][x] = getColor(heightMap[y][x])
+
+    image = Image.fromarray(colourMap, 'RGB')
     
-      current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-      try:
-            lastfilename = os.listdir(current_dir + "/maps")[-1]
-            newindex = int(lastfilename[3:-4]) + 1
-      except:
-            newindex = 0
+    try:
+        lastfilename = os.listdir(current_dir + "/maps")[-1]
+        newindex = int(lastfilename[3:-4]) + 1
+    except:
+      newindex = 0
     
+    image.save(current_dir + "/maps/Map" + str(newindex) + ".png")
+    return image
 
-      imnoise.save(current_dir + "/maps/noise" + str(newindex) + ".png")
-      imgradient.save(current_dir + "/maps/gradient" + str(newindex) + ".png")
-      imblended.save(current_dir + "/maps/blended" + str(newindex) + ".png")
-      print("  ...Main Process Done!...")
+def init(WIDTH = 256, HEIGHT = 256, SEED = uuid.uuid1().int >> 64, BIAS = 10, OCTAVES = 1, PERSISTANCE = 0.5, LACUNARITY = 2):
+  SCALE = max(WIDTH, HEIGHT) / 4
+  generateMap(HEIGHT, WIDTH, SEED, SCALE, OCTAVES, BIAS, PERSISTANCE, LACUNARITY, "MapSeed" + str(SEED))
 
-
-if __name__ == '__main__':
-    main()
-
-
-print("...Done!")
+if __name__ == "__main__":
+    init(OCTAVES = 5)
